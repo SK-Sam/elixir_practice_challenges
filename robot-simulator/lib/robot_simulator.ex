4f483ef2c43d6bd16a50b34914b7cdfa862e1432
Sam Yeo <yeosamuel95@gmail.com>
2021-04-04 07:26:59 -0600

    Pass initial test

---
+++ robot-simulator/lib/robot_simulator.ex
@@ -0,0 +1,49 @@
+defmodule RobotSimulator do
+  @doc """
+  Create a Robot Simulator given an initial direction and position.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec create(direction :: atom, position :: {integer, integer}) :: any
+  def create(direction \\ nil, position \\ nil) do
+    robot_position = if position == nil do
+      {0, 0}
+    else
+      position
+    end
+
+    robot_direction = if direction == nil do
+      :north
+    else
+      direction
+    end
+    %{position: robot_position, direction: robot_direction}
+  end
+
+  @doc """
+  Simulate the robot's movement given a string of instructions.
+
+  Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
+  """
+  @spec simulate(robot :: any, instructions :: String.t()) :: any
+  def simulate(robot, instructions) do
+  end
+
+  @doc """
+  Return the robot's direction.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec direction(robot :: any) :: atom
+  def direction(robot) do
+    robot.direction
+  end
+
+  @doc """
+  Return the robot's position.
+  """
+  @spec position(robot :: any) :: {integer, integer}
+  def position(robot) do
+    robot.position
+  end
+end
