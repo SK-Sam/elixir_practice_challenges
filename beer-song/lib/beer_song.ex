392de1ec0173e9bd8aebda6bf4a276fe21e78a43
Sam Yeo <yeosamuel95@gmail.com>
2021-04-18 04:10:50 -0600

    Complete verse function for all cases

--- beer-song/lib/beer_song.ex
+++ beer-song/lib/beer_song.ex
@@ -3,8 +3,20 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t()
-  def verse(number) do
+  def verse(number) when number > 2 do
     "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+  end
+
+  def verse(number) when number == 2 do
+    "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+  end
+
+  def verse(number) when number == 1 do
+    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+  end
+
+  def verse(number) when number == 0 do
+    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
 
   @doc """
