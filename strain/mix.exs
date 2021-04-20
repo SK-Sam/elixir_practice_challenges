ca37310b8b5c46c2ff660bc2f14a3b975008d13f
Sam Yeo <yeosamuel95@gmail.com>
2021-04-20 01:20:30 -0600

    🎉 Start strain challenge

--- strain/mix.exs
+++ strain/mix.exs
@@ -1,9 +1,9 @@
-defmodule RobotSimulator.MixProject do
+defmodule Strain.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :robot_simulator,
+      app: :strain,
       version: "0.1.0",
       # elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
